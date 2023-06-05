d05e7668c94516279b4145889e3bddf577ebe64b
Tu Nombre <you@example.com>
2023-06-05 13:52:16 -0500

    [FIX] eliminado filtro

--- l10n_do_debit_note/models/account_move.py
+++ l10n_do_debit_note/models/account_move.py
@@ -72,7 +72,7 @@ class AccountMove(models.Model):
         """
 
         debit_notes = self.search(
-            [("debit_note", "=", True), ("debit_origin_id", "=", False)]
+            [("debit_origin_id", "=", False)]
         )
 
         for dn in debit_notes:
